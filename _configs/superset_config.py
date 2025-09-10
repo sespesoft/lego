49d3312f3bb4fcf697d0713c992ec4af7bd93dfb
Marlon Ramírez <marlonramirez@outlook.com>
2025-09-10 08:31:54 -0500

    feat: include cors origin grafana and superset

--- _configs/superset_config.py
+++ _configs/superset_config.py
@@ -1,17 +1,15 @@
 import os
-
+from celery.schedules import crontab
 
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "postgres")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.environ.get("POSTGRES_DB")
-
 SQLALCHEMY_DATABASE_URI = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
     f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 )
-
 SECRET_KEY = os.environ.get("SUPERSET_SECRET_KEY")
 REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PORT = os.environ.get("REDIS_PORT", "6379")
@@ -23,11 +21,27 @@ CACHE_CONFIG = {
     "CACHE_KEY_PREFIX": "superset_cache_",
     "CACHE_REDIS_URL": f"{REDIS_URL}/0",
 }
-RESULTS_BACKEND = {
-    "CACHE_TYPE": "RedisCache",
-    "CACHE_DEFAULT_TIMEOUT": 86400,  # 24 horas
-    "CACHE_KEY_PREFIX": "superset_results_",
-    "CACHE_REDIS_URL": f"{REDIS_URL}/1",
+
+class CeleryConfig:
+    broker_url = f"{REDIS_URL}/1"
+    imports = ("superset.sql_lab",)
+    result_backend = f"{REDIS_URL}/2"
+    worker_prefetch_multiplier = 10
+    task_track_started = True
+    task_acks_late = True
+    beat_schedule = {
+        "reports.scheduler": {
+            "task": "reports.scheduler",
+            "schedule": crontab(minute="*", hour="*"),
+        },
+        "reports.prune_log": {
+            "task": "reports.prune_log",
+            "schedule": crontab(minute=10, hour=0),
+        },
+    }
+CELERY_CONFIG = CeleryConfig
+FEATURE_FLAGS = {
+    "ENABLE_TEMPLATE_PROCESSING": True,
+    "ALLOW_FILE_UPLOAD": True,
 }
-RATELIMIT_STORAGE_URI = f"{REDIS_URL}/2"
-ENABLE_FILE_UPLOAD = True
+RATELIMIT_STORAGE_URI = f"{REDIS_URL}/3"
--- _configs/superset_entrypoint.sh
+++ _configs/superset_entrypoint.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
-
 set -e
 
-INIT_FLAG="/app/superset_home/.initialized"
-
-if [ ! -f "$INIT_FLAG" ]; then
-  echo ">> First time running. Initializing Superset..."
-  echo ">> Upgrading Superset database..."
-  superset db upgrade
-  echo ">> Creating admin user..."
-  superset fab create-admin \
-              --username ${ADMIN_USERNAME} \
-              --firstname ${ADMIN_FIRSTNAME} \
-              --lastname ${ADMIN_LASTNAME} \
-              --email ${ADMIN_EMAIL} \
-              --password ${ADMIN_PASSWORD}
-  echo ">> Initializing Superset..."
-  superset init
-  echo ">> Initialization complete. Creating flag file."
-  touch "$INIT_FLAG"
-else
-  echo ">> Initialization already done. Skipping."
-  superset db upgrade
-fi
-echo ">> Starting Superset web server..."
+superset db upgrade
+superset fab create-admin \
+            --username "${ADMIN_USERNAME}" \
+            --firstname "${ADMIN_FIRSTNAME}" \
+            --lastname "${ADMIN_LASTNAME}" \
+            --email "${ADMIN_EMAIL}" \
+            --password "${ADMIN_PASSWORD}"
+superset init
 exec /usr/bin/run-server.sh
